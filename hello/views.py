db4ba69b5b5e85f9c93ace67bfb18368e72cc742
sacko <sackosekou1113@gmail.com>
2021-07-07 11:58:07 +0200

    Demo

--- hello/views.py
+++ hello/views.py
@@ -1,3 +1,5 @@
+import os
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -9,9 +11,11 @@ import requests
     # return HttpResponse('Hello from Python!')
     #return render(request, "index.html")
 def index(request):
-    r = requests.get('http://httpbin.org/status/418')
-    print(r.text)
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    #r = requests.get('http://httpbin.org/status/418')
+    #print(r.text)
+    #return HttpResponse('<pre>' + r.text + '</pre>')
+    times = int(os.environ.get('TIMES',3))
+    return HttpResponse('Hello! ' * times)
 
 
 def db(request):
